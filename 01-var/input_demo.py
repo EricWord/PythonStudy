597c5b6deffb7b139d0356b6be7c3344fc5674bb
“cuigs” <cuiguangsong@miliantech.com>
2021-03-02 16:52:39 +0800

    input

---
+++ 01-var/input_demo.py
@@ -0,0 +1,11 @@
+# name = input("请输入姓名：")
+# print(type(name))
+# print(3 ** 3)
+# print(81 ** 0.5)
+print('hello' * 2)
+m, n = 3, 5
+print(m, n)
+o, *p, q = 1, 2, 3, 4, 5, 6
+print(o, p, q)
+
+print(1 and 2 and 3)
