96cc7c93c8a440b9b9710fa72ae9080da8d1c4b7
“cuigs” <cuiguangsong@miliantech.com>
2021-03-02 14:32:01 +0800

    变量和数据类型、标识符命名规范

---
+++ 01-var/VarDemo.py
@@ -0,0 +1,10 @@
+a = 34
+b = 'hello'
+c = True
+
+d = ['周杰伦', '张三', '李四']
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'str'>
+print(type(c))  # <class 'bool'>
+print(type(d))  # <class 'list'>
+
