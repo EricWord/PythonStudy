3f208070de31293f31ed3c51776a785eefab4dd4
“cuigs” <cuiguangsong@miliantech.com>
2021-03-03 09:53:07 +0800

    str

---
+++ 04-for/break_demo.py
@@ -0,0 +1,6 @@
+i = 0
+while i < 5:
+    if i == 3:
+        continue
+        i += 1
+    print(i)
---
+++ 05-str/str_demo.py
@@ -0,0 +1,6 @@
+word = 'zhangsan'
+print(word[4])
+word[4] = 'a'
+print(word[4])
+word.find('g')
+
