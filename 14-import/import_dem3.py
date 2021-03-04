672857afc929d7afb2fa8685988d943740037a65
“cuigs” <cuiguangsong@miliantech.com>
2021-03-04 17:06:26 +0800

    math模块

---
+++ 14-import/import_dem3.py
@@ -0,0 +1,8 @@
+# sys 系统相关的功能
+import sys
+# 结果是一个列表，表示查找模块的路径
+print(sys.path)
+
+# sys.stdin 接收用户的输入，和input相关
+# sys.stdout 可以设置默认输出位置
+# sys.stderr 可以设置错误信息的默认输出位置
---
+++ 14-import/import_demo4.py
@@ -0,0 +1,13 @@
+import math
+
+# 求阶乘
+print(math.factorial(6))
+# 向下取整
+print(math.floor(12.98))
+# 向上取整
+print(math.ceil(15.0001))
+# 幂运算
+print(math.pow(2, 10))
+print(math.sin(math.pi / 6))
+print(math.cos(math.pi / 3))
+print(math.tan(math.pi / 2))
